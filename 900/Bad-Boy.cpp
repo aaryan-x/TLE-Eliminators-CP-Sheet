fc8a48ead3c803ab93735cf4ea7b0c8b89f87df5
Aaryan Sharma <82556028+aaryan-x@users.noreply.github.com>
2024-04-14 01:29:00 +0530

    Create Bad-Boy.cpp

---
+++ 900/Bad-Boy.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+void fast() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void solve(){
+    ll n, m, i, j;
+    cin >> n >> m >> i >> j;
+    cout << 1 << " " << 1 << " " << n << " " << m << endl;
+}
+
+int main() {
+    fast();
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
