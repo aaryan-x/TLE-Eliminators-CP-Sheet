8bc84f52d36b2cc75713b0a4d02c5f6e1610d8ae
Aaryan Sharma <82556028+aaryan-x@users.noreply.github.com>
2024-04-16 03:58:45 +0530

    Create Make-It-Zero.cpp

---
+++ 900/Make-It-Zero.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+void fast() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void solve() {
+    ll n;
+    cin >> n;
+    ll arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    if (n % 2 == 0) {
+        cout << 2 << endl;
+        cout << 1 << " " << n << endl;
+        cout << 1 << " " << n << endl;
+    } else {
+        cout << 4 << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << n - 1 << " " << n << endl;
+        cout << n - 1 << " " << n << endl;
+    }
+}
+
+int main() {
+    fast();
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
