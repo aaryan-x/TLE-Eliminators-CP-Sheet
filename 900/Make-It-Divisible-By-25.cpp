411edf5e30d20da1c4d0d26b20cc3ba5e04fcbb3
Aaryan Sharma <82556028+aaryan-x@users.noreply.github.com>
2025-09-05 11:27:31 +0530

    Update Make-It-Divisible-By-25.cpp

--- 900/Make-It-Divisible-By-25.cpp
+++ 900/Make-It-Divisible-By-25.cpp
@@ -8,38 +8,44 @@ void fast() {
     cout.tie(NULL);
 }
 
+vector<string> vals = {"00", "25", "50", "75"};
+
 void solve() {
     string str;
     cin >> str;
     int n = str.size();
-    int minDel = n;
+    int minDel = INT_MAX;
 
-    for (int i = n - 1; i >= 0; --i) {
-        if (str[i] == '0') {
-            for (int j = i - 1; j >= 0; --j) {
-                if (str[j] == '0' || str[j] == '5') {
-                    minDel = min(minDel, (n - 2 - j));
-                    break;
-                }
-            }
-        } else if (str[i] == '5') {
-            for (int j = i - 1; j >= 0; --j) {
-                if (str[j] == '2' || str[j] == '7') {
-                    minDel = min(minDel, (n - 2 - j));
-                    break;
-                }
-            }
+    for (auto val : vals) {
+        int ptr = n - 1, ans = 0;
+
+        while (ptr >= 0 && str[ptr] != val[1]) {
+            ptr--;
+            ans++;
         }
+        if (ptr < 0) {
+            ans = INT_MAX;
+            continue;
+        }
+
+        ptr--;
+        while (ptr >= 0 && str[ptr] != val[0]) {
+            ptr--;
+            ans++;
+        }
+
+        if (ptr >= 0) minDel = min(minDel, ans);
     }
-    
-    cout << minDel << endl;
+
+    if (minDel == INT_MAX) cout << -1 << "\n";
+    else cout << minDel << "\n";
 }
 
-int main(){
+int main() {
     fast();
     int t;
     cin >> t;
-    while(t--){
+    while (t--) {
         solve();
     }
     return 0;
