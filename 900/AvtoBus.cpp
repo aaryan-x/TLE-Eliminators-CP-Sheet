772222bf32f2cfb37fe73ff54d2c10c1bff456f2
Aaryan Sharma <82556028+aaryan-x@users.noreply.github.com>
2025-09-09 01:26:57 +0530

    Update AvtoBus.cpp

--- 900/AvtoBus.cpp
+++ 900/AvtoBus.cpp
@@ -1,26 +1,25 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 typedef long long int ll;
 
-void fast(){
+void fast() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 }
 
-void solve(){
-    ll n;
-    cin >> n;
-    if (n % 2 == 0 && n >= 4) {
-        if (n%6 == 0) {
-            cout << n/6 << " ";
-        } else {
-            cout << n/6 + 1 << " ";
-        }
-
-        cout << n/4 << endl;
-    } else cout << -1 << endl;
+void solve() {
+    ll wheels;
+    cin >> wheels;
+    
+    if (wheels % 2 != 0 || wheels < 4) {
+        cout << "-1\n";
+        return;
+    }
+    
+    ll mnBuses = (wheels + 5) / 6;  // ceil(wheels/6)
+    ll mxBuses = wheels / 4;
+    cout << mnBuses << " " << mxBuses << "\n";
 }
 
 int main() {
